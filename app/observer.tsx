d6047532ecfdc91074191511d98f5f756926b11f
K.Yamakawa <87867174+lCyou@users.noreply.github.com>
2023-05-16 22:54:37 +0900

    on hold observer

--- app/observer.tsx
+++ app/observer.tsx
@@ -1,5 +1,5 @@
-import { useInView } from 'react-intersection-observer';
+// import { useInView } from 'react-intersection-observer';
 
-const [ref, inView] = useInView({
-    rootMargin: '-100px 0px',
-  });
+// const [ref, inView] = useInView({
+//     rootMargin: '-100px 0px',
+//   });
